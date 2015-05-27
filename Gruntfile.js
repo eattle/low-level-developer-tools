ecaf2d221e49aeaa72a68e4034d162b75dac4b28
류형욱 <mintrupt@mintrupt.com>
2015-05-28 04:05:21 +0900

    더 좋은 빌드 과정

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,24 @@ module.exports = function (grunt) {
   grunt.initConfig({
     // Metadata.
     pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.title || pkg.name %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author %> */\n',
     // Task configuration.
+    concat: {
+      options: {
+        banner: '<%= banner %>' + '\'use strict\';\n',
+        process: function (src) {
+          return '!function(){' + src + '}();';
+        },
+        stripBanners: true
+      },
+      app: {
+        src: ['src/js/**/*.js'],
+        dest: 'app/scripts.js'
+      }
+    },
     mochaTest: {
       test: {
         options: {
@@ -15,12 +32,43 @@ module.exports = function (grunt) {
         src: 'test/'
       }
     },
+    jade: {
+      app: {
+        files: [{
+          cwd: 'src',
+          src: '**/*.jade',
+          dest: 'app',
+          expand: true,
+          ext: '.html'
+        }]
+      }
+    },
+    stylus: {
+      compile: {
+        files: [{
+          cwd: 'src',
+          src: '**/*.styl',
+          dest: 'app',
+          expand: true,
+          ext: '.css'
+        }]
+      }
+    },
+    uglify: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      app: {
+        src: '<%= concat.app.dest %>',
+        dest: 'app/scripts.min.js'
+      }
+    },
     jshint: {
       gruntfile: {
         src: 'Gruntfile.js'
       },
-      app: {
-        src: 'app/js/**/*.js'
+      js: {
+        src: 'src/js/**/*.js'
       },
       test: {
         src: 'test/**/*.js',
@@ -46,7 +94,16 @@ module.exports = function (grunt) {
         esnext: true,
         browser: false,
         node: true,
-        quotmark: 'single'
+        quotmark: 'single',
+        globals: {
+          'angular': false,
+          'console': false,
+          'moment': false,
+          '_': false,
+          'swal': false,
+          'alert': false,
+          '$': false
+        }
       }
     },
     nodewebkit: {
@@ -56,6 +113,9 @@ module.exports = function (grunt) {
         buildDir: './webkitbuilds'
       },
       src: ['./app/**/*']
+    },
+    clean: {
+      src: ['app/*', '!app/bower_components/**', '!app/package.json']
     }
   });
 
@@ -63,8 +123,15 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-mocha-test');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-node-webkit-builder');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-jade');
+  grunt.loadNpmTasks('grunt-contrib-stylus');
+
+  grunt.registerTask('common', ['clean', 'jshint', 'concat', 'uglify', 'jade', 'stylus']);
 
   // Default task.
-  grunt.registerTask('default', ['test', 'nodewebkit']);
+  grunt.registerTask('default', ['test', 'common', 'nodewebkit']);
   grunt.registerTask('test', ['jshint', 'mochaTest']);
 };
